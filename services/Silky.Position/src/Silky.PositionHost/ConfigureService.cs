e25ff1737f22c77d6d8d148712b59eeb2900fed8
刘洪亮 <liuhongliang@jingshonline.com>
2021-11-24 13:28:27 +0800

    新增Position微服务应用

---
+++ services/Silky.Position/src/Silky.PositionHost/ConfigureService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Silky.Position.EntityFrameworkCore.DbContexts;
+
+namespace Silky.PositionHost
+{
+    public class ConfigureService : IConfigureService
+    {
+        public void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSilkyCaching()
+                .AddSilkySkyApm()
+                .AddMessagePackCodec();
+            
+            services.AddDatabaseAccessor(
+                options => { options.AddDbPool<DefaultContext>(); },
+                "Silky.Position.Database.Migrations");
+        }
+    }
+}
