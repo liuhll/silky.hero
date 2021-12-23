02498263536b223b604c1a1bd7b2a3e37a65fc6d
liuhll <1029765111@qq.com>
2021-12-23 15:05:09 +0800

    新增Permission微服务

---
+++ services/Silky.Permission/src/Silky.PermissionHost/ConfigureService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Silky.Permission.EntityFrameworkCore.DbContexts;
+
+namespace Silky.PermissionHost
+{
+    public class ConfigureService : IConfigureService
+    {
+        public void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSilkyCaching()
+                .AddSilkySkyApm()
+                .AddMessagePackCodec();
+            
+            services.AddDatabaseAccessor(
+                options => { options.AddDbPool<DefaultContext>(); },
+                "Silky.Permission.Database.Migrations");
+        }
+    }
+}
---
+++ services/Silky.Permission/src/Silky.PermissionHost/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace Silky.PermissionHost
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            await CreateHostBuilder(args).Build().RunAsync();
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureSilkyWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+    }
+}
