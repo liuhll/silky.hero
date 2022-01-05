e79b23220653ac8a393777e95e67da893217828b
liuhll <1029765111@qq.com>
2022-01-05 16:05:15 +0800

    优化组织机构实体属性映射配置

---
+++ services/Silky.Organization/src/Silky.Organization.Domain/Organization/OrganizationTypeBuilder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Silky.EntityFrameworkCore.Entities.Configures;
+using Silky.Hero.Common.EntityFrameworkCore.Modeling;
+using Silky.Hero.Common.Enums;
+
+namespace Silky.Organization.Domain;
+
+public class OrganizationTypeBuilder : IEntityTypeBuilder<Organization>
+{
+    public void Configure(EntityTypeBuilder<Organization> builder, DbContext dbContext, Type dbContextLocator)
+    {
+        builder.ToTable(OrganizationDbProperties.DbTablePrefix + "Organizations", OrganizationDbProperties.DbSchema);
+        builder.ConfigureByConvention();
+        builder.Property(o => o.ParentId)
+            .HasColumnName(nameof(Domain.Organization.ParentId));
+        builder.Property(o => o.Name)
+            .IsRequired()
+            .HasMaxLength(OrganizationConsts.MaxNameLength)
+            .HasColumnName(nameof(Domain.Organization.Name));
+        builder.Property(o => o.Code)
+            .IsRequired()
+            .HasMaxLength(OrganizationConsts.MaxCodeLength)
+            .HasColumnName(nameof(Domain.Organization.Code));
+        builder.Property(o => o.Sort)
+            .HasDefaultValue(0)
+            .HasColumnName(nameof(Domain.Organization.Sort));
+        builder.Property(o => o.Remark)
+            .HasMaxLength(OrganizationConsts.MaxRemarkLength)
+            .HasColumnName(nameof(Domain.Organization.Remark));
+        builder.Property(o => o.Status)
+            .IsRequired()
+            .HasDefaultValue(Status.Valid)
+            .HasColumnName(nameof(Domain.Organization.Status));
+        builder.HasMany(o => o.Children)
+            .WithOne(o => o.Parent)
+            .HasForeignKey(o => o.ParentId);
+    }
+}
--- services/Silky.Organization/src/Silky.Organization.EntityFrameworkCore/DbContexts/DefaultDbContext.cs
+++ services/Silky.Organization/src/Silky.Organization.EntityFrameworkCore/DbContexts/DefaultDbContext.cs
@@ -17,7 +17,6 @@ namespace Silky.Organization.EntityFrameworkCore.DbContexts
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
-            builder.ConfigureOrganization();
         }
     }
 }
