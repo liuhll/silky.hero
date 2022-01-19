b8af65d77da9844d5207e6fe374cf1006541e9ee
liuhll <1029765111@qq.com>
2022-01-19 22:18:50 +0800

    更新组织机构模块

---
+++ frontend/src/views/system-manage/organization/OrganizationModal.vue
@@ -0,0 +1,16 @@
+<template>
+  
+</template>
+
+<script>
+import { defineComponent, ref, computed, unref } from 'vue';
+import { BasicModal, useModalInner } from '/@/components/Modal';
+import { BasicForm, useForm } from '/@/components/Form/index';
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- frontend/src/views/system-manage/organization/index.vue
+++ frontend/src/views/system-manage/organization/index.vue
@@ -3,6 +3,9 @@
     <Row :gutter="[24, 16]">
       <Col :span="6">
         <Card title="组织机构树">
+          <template #extra>
+            <a-button type="primary" @click="handleCreateOrganizationRoot">添加根机构</a-button>
+          </template>
           <BasicTree
             defaultExpandAll
             :clickRowToExpand="false"
@@ -15,7 +18,7 @@
       </Col>
       <Col :span="18">
         <Card title="成员" class="w-4/4 xl:w-5/5">
-          <BasicTable @register="registerTable" class="w-4/4 xl:w-5/5" :searchInfo="searchInfo">
+          <BasicTable @register="registerTable" class="w-4/4 xl:w-5/5" :searchInfo="searchInfo" ref="tableRef">
             <template #toolbar>
               <a-button type="primary" @click="handleCreate" v-if="selectedOrganization"
                 >添加成员</a-button
@@ -36,15 +39,16 @@
                 ]"
               />
             </template>
-          </BasicTable> </Card
-      ></Col>
+          </BasicTable>
+        </Card>
+     </Col>
     </Row>
   </PageWrapper>
 </template>
 <script lang="ts">
 import { PageWrapper } from '/@/components/Page';
 import { Card, Row, Col } from 'ant-design-vue';
-import { BasicTable, useTable, TableAction } from '/@/components/Table';
+import { BasicTable, useTable, TableAction, TableActionType } from '/@/components/Table';
 import { BasicTree, TreeActionType, TreeItem, ContextMenuItem } from '/@/components/Tree';
 import { defineComponent, ref, unref, onMounted, reactive } from 'vue';
 import { getOrganizationTree, getOrganizationUserPageList } from '/@/api/organization';
@@ -56,8 +60,10 @@ export default defineComponent({
   components: { Card, Row, Col, PageWrapper, BasicTree, BasicTable, TableAction },
   setup() {
     const treeRef = ref<Nullable<TreeActionType>>(null);
+    const tableRef = ref<Nullable<TableActionType>>(null);
     const treeData = ref<TreeItem[]>([]);
     const selectedOrganization = ref<boolean>(false);
+    const selectedOrganizationId = ref<number|undefined>();
     const searchInfo = reactive<Recordable>({});
     function getTree() {
       const tree = unref(treeRef);
@@ -67,23 +73,42 @@ export default defineComponent({
       return tree;
     }
 
+    function getTable() {
+      const table = unref(tableRef);
+      if (!table) {
+        throw new Error('table is null!');
+      }
+      return table;
+    }
+
     const [registerTable, { reload, updateTableDataRecord }] = useTable({
       rowKey: 'id',
       columns: userColumns,
       api: getOrganizationUserPageList,
       immediate: false,
-     
+      locale: {
+        emptyText: '选择一个组织机构来查看成员'
+      }
     });
     onMounted(async () => {
       await loadOrganizationTreeData();
       getTree().expandAll(true);
     });
 
-    function handleSelect(key: number) {
-      selectedOrganization.value = true;
-      debugger;
-      searchInfo.id = key;
-      reload();
+    function handleSelect(keys: number[]) {
+      if (keys.length > 0) {
+        const orgId = keys[0];
+        if (orgId != selectedOrganization.value) {
+          selectedOrganization.value = orgId;
+          searchInfo.id = orgId;
+          getTable().setProps({
+            locale: {
+              emptyText: '暂无数据'
+            }
+          });
+          reload();
+        }
+      }
     }
 
     async function loadOrganizationTreeData() {
@@ -101,9 +126,16 @@ export default defineComponent({
     function getRightMenuList(node: any): ContextMenuItem[] {
       return [
         {
-          label: '新增',
+          label: '编辑',
           handler: () => {
             console.log('点击了新增', node);
+          },
+          icon: 'clarity:note-edit-line',
+        },
+         {
+          label: '添加子机构',
+          handler: () => {
+            console.log('添加子机构', node);
           },
           icon: 'bi:plus',
         },
@@ -119,6 +151,7 @@ export default defineComponent({
     return {
       treeData,
       treeRef,
+      tableRef,
       selectedOrganization,
       searchInfo,
       getRightMenuList,
