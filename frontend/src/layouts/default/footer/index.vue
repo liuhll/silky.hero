db678d0af308bac6c19760b560d8f3111711ff07
liuhll <1029765111@qq.com>
2022-02-11 09:08:55 +0800

    更改网站名称

--- frontend/src/layouts/default/footer/index.vue
+++ frontend/src/layouts/default/footer/index.vue
@@ -7,7 +7,7 @@
 
       <a @click="openWindow(DOC_URL)">{{ t('layout.footer.onlineDocument') }}</a>
     </div>
-    <div>Copyright &copy;2020 Vben Admin</div>
+    <div>Copyright &copy;2022 Silky Hero</div>
   </Footer>
 </template>
 
