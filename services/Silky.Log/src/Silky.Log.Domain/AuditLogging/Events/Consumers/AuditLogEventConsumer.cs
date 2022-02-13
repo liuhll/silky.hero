c43f0c2a5b1706132871779ddd2c8e3b99309b87
liuhll <1029765111@qq.com>
2022-02-13 21:36:51 +0800

    优化写审计日志方法

--- services/Silky.Log/src/Silky.Log.Domain/AuditLogging/Events/Consumers/AuditLogEventConsumer.cs
+++ services/Silky.Log/src/Silky.Log.Domain/AuditLogging/Events/Consumers/AuditLogEventConsumer.cs
@@ -9,16 +9,11 @@ namespace Silky.Log.Domain.AuditLogging.Events.Consumers;
 
 public class AuditLogEventConsumer: IConsumer<AuditLogInfo>
 {
-    private readonly IRepository<AuditLog> _auditLogRepository;
-
-    public AuditLogEventConsumer()
-    {
-        _auditLogRepository = EngineContext.Current.Resolve<IRepository<AuditLog>>();
-    }
-
+    
     public async Task Consume(ConsumeContext<AuditLogInfo> context)
     {
+        var auditLogRepository = EngineContext.Current.Resolve<IRepository<AuditLog>>();
         var auditLog = context.Message.Adapt<AuditLog>();
-        await _auditLogRepository.InsertNowAsync(auditLog);
+        await auditLogRepository.InsertNowAsync(auditLog);
     }
 }
