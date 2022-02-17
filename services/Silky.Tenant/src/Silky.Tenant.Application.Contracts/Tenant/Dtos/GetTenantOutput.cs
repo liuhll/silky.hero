a2a2bcbe74124e41ba32ee0f8f5a87f9f569a4a4
liuhll <1029765111@qq.com>
2022-02-18 00:17:50 +0800

    查询返回结果新增创建时间和更新时间

--- services/Silky.Tenant/src/Silky.Tenant.Application.Contracts/Tenant/Dtos/GetTenantOutput.cs
+++ services/Silky.Tenant/src/Silky.Tenant.Application.Contracts/Tenant/Dtos/GetTenantOutput.cs
@@ -5,4 +5,8 @@ namespace Silky.Tenant.Application.Contracts.Tenant.Dtos;
 public class GetTenantOutput : TenantDtoBase
 {
     public long Id { get; set; }
+    
+    public DateTimeOffset CreatedTime { get; set; }
+    
+    public DateTimeOffset? UpdatedTime { get; set; }
 }
--- services/Silky.Tenant/src/Silky.Tenant.Application.Contracts/Tenant/Dtos/GetTenantPageOutput.cs
+++ services/Silky.Tenant/src/Silky.Tenant.Application.Contracts/Tenant/Dtos/GetTenantPageOutput.cs
@@ -1,6 +1,10 @@
-﻿namespace Silky.Tenant.Application.Contracts.Tenant.Dtos;
+﻿using System;
+
+namespace Silky.Tenant.Application.Contracts.Tenant.Dtos;
 
 public class GetTenantPageOutput : GetTenantOutput
 {
+    public DateTimeOffset CreatedTime { get; set; }
     
+    public DateTimeOffset? UpdatedTime { get; set; }
 }
