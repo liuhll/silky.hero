8097a11362b92b818dfb528264262eed7770e777
liuhll <1029765111@qq.com>
2022-03-02 20:35:06 +0800

    修复组织机构API接口配置错误的bug

--- frontend/src/api/organization/index.ts
+++ frontend/src/api/organization/index.ts
@@ -7,12 +7,11 @@ import {
   CreateOrgizationModel,
 } from './model/organizationModel';
 import { BasicFetchResult } from '/@/api/model/baseModel';
-import { requestParams } from '../../../mock/_util';
 enum Api {
   GetOrganizationTree = '/organization/tree',
   Organization = '/organization',
-  GetAllocationOrganizationRoles = 'organization/role/allocation/list',
-  GetAllocationOrganizationPositions = 'organization/position/allocation/list',
+  GetAllocationOrganizationRoles = '/organization/role/allocation/list',
+  GetAllocationOrganizationPositions = '/organization/position/allocation/list',
 }
 
 export const getOrganizationTree = () => {
