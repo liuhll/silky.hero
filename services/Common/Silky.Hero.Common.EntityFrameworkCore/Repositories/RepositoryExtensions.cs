c3ce043ec4f6b5d6d1ad44973050fa230aa49edd
liuhll <1029765111@qq.com>
2022-11-15 15:00:28 +0800

    修复仓库扩展方法存在的bug

--- services/Common/Silky.Hero.Common.EntityFrameworkCore/Repositories/RepositoryExtensions.cs
+++ services/Common/Silky.Hero.Common.EntityFrameworkCore/Repositories/RepositoryExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Silky.Account.Application.Contracts.Account;
 using Silky.Core;
+using Silky.Core.Threading;
 using Silky.EntityFrameworkCore.Entities;
 using Silky.Hero.Common.EntityFrameworkCore.Entities;
 using Silky.Hero.Common.Session;
@@ -9,37 +10,12 @@ namespace Silky.EntityFrameworkCore.Repositories;
 
 public static class RepositoryExtensions
 {
-    public static async Task<IQueryable<TEntity>> GetCurrentUserDataRangesAsync<TEntity>(this IQueryable<TEntity> query)
-        where TEntity : class, IHasOrganization, IEntity, new()
-    {
-        var accountAppService = EngineContext.Current.Resolve<IAccountAppService>();
-        var currentUserDataRange = await accountAppService.GetCurrentUserDataRangeAsync();
-        if (currentUserDataRange.IsAllData)
-        {
-            return query;
-        }
-
-        if (currentUserDataRange.OrganizationIds.Count() == 1)
-        {
-            return query.Where(p => p.OrganizationId == currentUserDataRange.OrganizationIds.First());
-        }
-
-        return query.Where(p => currentUserDataRange.OrganizationIds.Contains(p.OrganizationId));
-    }
-
-    public static async Task<IQueryable<TEntity>> GetCurrentUserDataRangesAsync<TEntity>(
-        this IRepository<TEntity> repository, bool? tracking = false)
-        where TEntity : class, IHasOrganization, IEntity, new()
-    {
-        return await repository.AsQueryable(tracking).GetCurrentUserDataRangesAsync();
-    }
-
     public static IQueryable<TEntity> GetCurrentUserDataRanges<TEntity>(
         this IQueryable<TEntity> query, bool? tracking = false)
         where TEntity : class, IHasOrganization, IEntity, new()
     {
         var accountAppService = EngineContext.Current.Resolve<IAccountAppService>();
-        var currentUserDataRange = accountAppService.GetCurrentUserDataRangeAsync().GetAwaiter().GetResult();
+        var currentUserDataRange = AsyncHelper.RunSync(() => accountAppService.GetCurrentUserDataRangeAsync());
         if (currentUserDataRange.IsAllData)
         {
             return query;
@@ -61,42 +37,42 @@ public static class RepositoryExtensions
     }
 
     public static Task RealDeleteAsync<TEntity>(this IRepository<TEntity> repository, IEnumerable<TEntity> entities)
-        where TEntity : class, IHasOrganization, IEntity, new()
+        where TEntity : class, IEntity, new()
     {
         repository.DynamicContext.RealDeleteFlag = true;
         return repository.DeleteAsync(entities);
     }
 
     public static Task RealDeleteAsync<TEntity>(this IRepository<TEntity> repository, TEntity entity)
-        where TEntity : class, IHasOrganization, IEntity, new()
+        where TEntity : class, IEntity, new()
     {
         repository.DynamicContext.RealDeleteFlag = true;
         return repository.DeleteAsync(entity);
     }
 
     public static Task RealDeleteAsync<TEntity>(this IRepository<TEntity> repository, object key)
-        where TEntity : class, IHasOrganization, IEntity, new()
+        where TEntity : class, IEntity, new()
     {
         repository.DynamicContext.RealDeleteFlag = true;
         return repository.DeleteAsync(key);
     }
 
     public static Task RealDeleteNowAsync<TEntity>(this IRepository<TEntity> repository, IEnumerable<TEntity> entities)
-        where TEntity : class, IHasOrganization, IEntity, new()
+        where TEntity : class, IEntity, new()
     {
         repository.DynamicContext.RealDeleteFlag = true;
         return repository.DeleteNowAsync(entities);
     }
 
     public static Task RealDeleteNowAsync<TEntity>(this IRepository<TEntity> repository, TEntity entity)
-        where TEntity : class, IHasOrganization, IEntity, new()
+        where TEntity : class, IEntity, new()
     {
         repository.DynamicContext.RealDeleteFlag = true;
         return repository.DeleteNowAsync(entity);
     }
 
     public static Task RealDeleteNowAsync<TEntity>(this IRepository<TEntity> repository, object key)
-        where TEntity : class, IHasOrganization, IEntity, new()
+        where TEntity : class, IEntity, new()
     {
         repository.DynamicContext.RealDeleteFlag = true;
         return repository.DeleteNowAsync(key);
@@ -104,7 +80,7 @@ public static class RepositoryExtensions
 
     public static IQueryable<TEntity> GetAllIncludeSoftDeletedAsync<TEntity>(this IRepository<TEntity> repository,
         bool? tracking = false)
-        where TEntity : class, IHasOrganization, IEntity, new()
+        where TEntity : class, IEntity, new()
     {
         var query = repository.AsQueryable(tracking).IgnoreQueryFilters();
 
