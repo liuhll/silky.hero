0b2c336f0487e28c58dad55275ea9a0ee5aa5aa9
liuhll <1029765111@qq.com>
2023-02-24 15:55:39 +0800

    优化主函数的编写

--- services/Shared/CSharpScripts/Program.cs
+++ services/Shared/CSharpScripts/Program.cs
@@ -1,11 +1,8 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Silky.GatewayHost;
-
 
 var hostBuilder = Host.CreateDefaultBuilder(args)
-    .ConfigureSilkyGatewayDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); })
+    .ConfigureSilkyGeneralHostDefaults()
     .UseNacosConfig("NacosConfig", Nacos.YamlParser.YamlConfigurationStringParser.Instance)
     .Build();
 await hostBuilder.RunAsync();
